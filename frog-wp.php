4bcf95d815ae011d46f0e53784689982000bfef4
Anh Tran <anhtnt@elightup.com>
2022-02-15 16:59:54 +0700

    Add default fallback to error_log if Frog socket server is not available

--- frog-wp.php
+++ frog-wp.php
@@ -16,11 +16,23 @@ defined( 'ABSPATH' ) || die;
 require __DIR__ . '/vendor/autoload.php';
 
 use WebSocket\Client;
+use Riimu\Kit\PHPEncoder\PHPEncoder;
 
 function f( ...$variables ) {
-	$message = json_encode( $variables );
-
-	$client = new Client( 'ws://127.0.0.1:1503' );
-	$client->binary( $message );
-	$client->close();
+	try {
+		// Try sending data to Frog socket server.
+		$output = wp_json_encode( $variables );
+		$client  = new Client( 'ws://127.0.0.1:1503' );
+		$client->binary( $output );
+		$client->close();
+	} catch( Exception $e ) {
+		// Log to default logger as a fallback. Make data formatted.
+		$encoder = new PHPEncoder;
+		$output  = [];
+		foreach ( $variables as $variable ) {
+			$output[] = $encoder->encode( $variable );
+		}
+		$output = "\n\n" . implode( "\n\n", $output ) . "\n";
+		error_log( $output );
+	}
 }
